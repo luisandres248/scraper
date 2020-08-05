965fa5de034bd3fdc8419f4c1ea7c1345a8ccc2e
luis marquez <luisandres248@gmail.com>
2020-08-05 19:01:12 -0300

    rev 2

--- data.py
+++ data.py
@@ -1,28 +1,4 @@
 import pandas as pd
 import xlrd
 
-df = pd.read_excel('data.xlsx')
-print(df)
-df.columns = ['price' , 'description']
-print(df)
-print(df.dtypes)
 
-list = []
-list2 = []
-for value in df['price']:
-    value = str(value)
-    value = value.replace(".", "")
-    value = value.replace(",", ".")
-    value = int(value)
-    list.append(value)
-for title in df['description']:
-    title = str(title)
-    list2.append(title)
-d = {'price':list,'description':list2}
-df2 = pd.DataFrame(d)
-print(df2)
-print(df2.dtypes)
-
-df2 = df2.sort_values(by='price')
-df2.reset_index(drop=True,inplace=True)
-print(df2)
--- scraper.py
+++ scraper.py
@@ -1,22 +1,40 @@
+import sqlite3
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 from openpyxl.workbook import Workbook
+import sqlite3
+
 
 #declaring first variables and url
 price2 = []
 titel2 = []
-input= 'guitarra-acustica'
+input= 'guitarra-acustica-gibson'
 url= 'https://listado.mercadolibre.com.ar/'
 
 #first scrap to collect data
 page = requests.get(url+input)
 soup = BeautifulSoup(page.content, 'html.parser')
-price = soup.find_all('span', class_='price__fraction')
-title = soup.find_all('span', class_='main-title')
-price2 = [p2.get_text() for p2 in price]
-title2 = [t2.get_text() for t2 in title]
-print(len(price2))
+tuples = []
+
+for result in soup.find_all('div', class_='item__info'):
+    price = result.find('span', class_='price__fraction').get_text()
+    price = str(price)
+    price = price.replace(".", "")
+    price = price.replace(",", ".")
+    title = result.find('span', class_='main-title').get_text()
+    try:
+        cuotas = result.find('span', class_='item-installments-text').get_text()
+    except:
+        cuotas = 0
+    try:
+        envio = result.find('span', class_='text-shipping').get_text()
+    except:
+        envio = 0
+    tuple = (int(price), str(title), str(cuotas), str(envio))
+    tuples.append(tuple)
+print(len(tuples))
 
 #function to get next page link
 def getlink2(soup):
@@ -30,27 +48,67 @@ url2 = getlink2(soup).get('href')
 page2 = requests.get(url2)
 soup2 = BeautifulSoup(page2.content, 'html.parser')
 
+
 #while loop to collect data from all the results pages including the last one with the else statement
-while getlink2(soup2) :
-    pricen = soup2.find_all('span', class_='price__fraction')
-    titlen = soup2.find_all('span', class_='main-title')
-    price2.extend(p2n.get_text() for p2n in pricen)
-    title2.extend(t2n.get_text() for t2n in titlen)
-    url2 = getlink2(soup2).get('href')
+while getlink2(soup2):
+    for result in soup2.find_all('div', class_='item__info'):
+        price = result.find('span', class_='price__fraction').get_text()
+        price = str(price)
+        price = price.replace(".", "")
+        price = price.replace(",", ".")
+        title = result.find('span', class_='main-title').get_text()
+        try:
+            cuotas = result.find('span', class_='item-installments-text').get_text()
+        except:
+            cuotas = 0
+        try:
+            envio = result.find('span', class_='text-shipping').get_text()
+        except:
+            envio = 0
+        tuple = (int(price), str(title), str(cuotas), str(envio))
+        tuples.append(tuple)
+    url2 = getlink2(soup2)
     page2 = requests.get(url2)
     soup2 = BeautifulSoup(page2.content, 'html.parser')
+
 else:
-    pricen = soup2.find_all('span', class_='price__fraction')
-    titlen = soup2.find_all('span', class_='main-title')
-    price2.extend(p2n.get_text() for p2n in pricen)
-    title2.extend(t2n.get_text() for t2n in titlen)
+    for result in soup2.find_all('div', class_='item__info'):
+        price = result.find('span', class_='price__fraction').get_text()
+        price = str(price)
+        price = price.replace(".", "")
+        price = price.replace(",", ".")
+        title = result.find('span', class_='main-title').get_text()
+        try:
+            cuotas = result.find('span', class_='item-installments-text').get_text()
+        except:
+            cuotas = 0
+        try:
+            envio = result.find('span', class_='text-shipping').get_text()
+        except:
+            envio = 0
+        tuple = (int(price), str(title), str(cuotas), str(envio))
+        tuples.append(tuple)
+
 
 #creating dataframe to dump data and write to xlsx for further use
-df = pd.DataFrame(title2 , price2)
+df = pd.DataFrame(tuples, columns=['price','title','cuotas','envio'])
 df.to_excel('data.xlsx')
-print(df.describe)
+print(df.describe())
 
+#creating a SQL database for further use
+connection = sqlite3.connect("data.db")
+crsr = connection.cursor()
+sql_command = """create table results (
+key INTEGER PRIMARY KEY AUTOINCREMENT,
+price Real,
+description VARCHAR(500),
+cuotas VARCHAR(30),
+envio VARCHAR(30));"""
+crsr.execute(sql_command)
+crsr.executemany("insert into results (price, description, cuotas, envio) values (?, ?, ?, ?)", tuples)
+connection.commit()
+crsr.execute("SELECT * FROM results")
+ans = crsr.fetchmany(6)
+print(ans)
 
-
-####################################
 
